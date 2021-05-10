b146da298f0419da9e7ea594b582c75ae5018eaa
VincentProg <74202475+VincentProg@users.noreply.github.com>
2021-05-10 08:07:19 +0200

    Initial commit

---
+++ ProjetFinal/Assets/Vincent/Scripts/UI/UI_MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_MainMenu : MonoBehaviour
+{
+
+    public string sceneGame;
+    public string sceneOptions;
+
+
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(sceneGame);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(sceneOptions);
+    }
+
+
+
+
+
+}

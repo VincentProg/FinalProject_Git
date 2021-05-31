3a3457ea31cc953a96ee28d04b0bc8fba6cb18c1
Noé <noeguillaumin@gmail.com>
2021-05-31 15:16:20 +0200

    U I F O N C T I O N E L
    finally. . .

--- ProjetFinal/Assets/Noe/Scripts/Timeline.cs
+++ ProjetFinal/Assets/Noe/Scripts/Timeline.cs
@@ -8,12 +8,7 @@ public class Timeline : MonoBehaviour
 
     public static Timeline _instance { get; private set;}
 
-    [SerializeField] private float enlargmentValue;
-
-    public GameObject h1, h2;
-
-    private Slider sh1, sh2;
-    private Vector3 defSize;
+    public Image i1, i2;
 
     private void Awake()
     {
@@ -23,31 +18,12 @@ public class Timeline : MonoBehaviour
 
     void Start()
     {
-        sh1 = h1.GetComponentInChildren<Slider>();
-        sh2 = h2.GetComponentInChildren<Slider>();
-
-        defSize = h1.GetComponent<RectTransform>().localScale;
-        if (enlargmentValue == 0)
-            enlargmentValue = 1.1f;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        sh1.value = (float)CombatSystem.instance.heros[0].health / CombatSystem.instance.heros[0].stats.health;
-        sh2.value = (float)CombatSystem.instance.heros[1].health / CombatSystem.instance.heros[1].stats.health;
-    }
 
-    public void Enlarge(GameObject HeroBoxUI)
-    {
-        HeroBoxUI.GetComponent<RectTransform>().localScale *= enlargmentValue;
-        HeroBoxUI.GetComponentInChildren<Image>().color = Color.blue;
-    }
-
-    public void ResetHeroBoxToDefaultSize(GameObject HeroBoxUI)
-    {
-        HeroBoxUI.GetComponent<RectTransform>().localScale = defSize;
-        HeroBoxUI.GetComponentInChildren<Image>().color = Color.white;
     }
 }
